613fbb8604cdd8fd4d6a4a479f011c8e33da99aa
Joshua Carrier <jcarrier0026@gmail.com>
2020-08-10 14:38:13 -0400

    I'm stupid.
    Here is animation.h.....

---
+++ animation.h
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+#include "vector2.h"
+
+enum class AnimationType { kIdle, kRunRight, kRunLeft };
+
+struct Animation {
+  AnimationType name;
+  int num_of_frames;
+  SDL_Rect source_rect;
+  int time_between_frames;
+  std::vector<SDL_Rect> image_rects;
+  std::string sprite_sheet_name;
+  bool once;
+  SDL_Renderer *renderer;
+  Vector2 offsets;
+
+  Animation &operator=(Animation new_animation);
+};
+
